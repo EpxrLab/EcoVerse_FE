3f2a5822ec40ec169e3da0f850f51beebbe82ee6
TienNM <ojtgrowth@sendo.vn>
2026-05-10 07:44:17 +0700

    'fix

--- EcoVerse_FE/src/roles/partnership/pages/partnershipDashboard/PartnershipDashboard.jsx
+++ EcoVerse_FE/src/roles/partnership/pages/partnershipDashboard/PartnershipDashboard.jsx
@@ -25,7 +25,6 @@ export default function PartnershipDashboard() {
       icon: FileText,
       color: "text-eco-blue",
       bg: "bg-eco-blue/10",
-      desc: `${stats.activeCampaigns} đang diễn ra`
     },
     {
       title: "Trường tham gia",
